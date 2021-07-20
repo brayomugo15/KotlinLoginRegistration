3b5d87c672ee17ad8173fcae6d159ee3cef0ba48
Brian Mugo <brayomugo2015@gmail.com>
2021-07-20 17:40:27 +0300

    - Progress bar in Login - Get auth email

--- app/src/main/java/com/project/kotlinloginregistration/HomeFragment.kt
+++ app/src/main/java/com/project/kotlinloginregistration/HomeFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.project.kotlinloginregistration.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding;
+    private lateinit var mAuth: FirebaseAuth;
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.bind(inflater.inflate(R.layout.fragment_home, container, false))
+        val view = binding.root
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.tvEmail.setText(mAuth.currentUser!!.email)
+
+        return view;
     }
 }
--- app/src/main/java/com/project/kotlinloginregistration/LoginFragment.kt
+++ app/src/main/java/com/project/kotlinloginregistration/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.project.kotlinloginregistration
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.NavHostFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.project.kotlinloginregistration.databinding.FragmentLoginBinding
@@ -15,6 +18,7 @@ class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding;
     private lateinit var mAuth: FirebaseAuth;
+    private lateinit var progressBar: ProgressBar;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +32,9 @@ class LoginFragment : Fragment() {
         binding.btn.setOnClickListener {
             login()
         }
+
+        progressBar = binding.progressbar;
+
         binding.tvRegistration.setOnClickListener {
             register()
         }
@@ -47,6 +54,7 @@ class LoginFragment : Fragment() {
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this.context, "Empty fields", Toast.LENGTH_SHORT).show();
         } else {
+            progressBar.visibility = View.VISIBLE
             login(email, password)
         }
     }
@@ -57,7 +65,9 @@ class LoginFragment : Fragment() {
                 if (task.isSuccessful) {
                     val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment();
                     NavHostFragment.findNavController(requireParentFragment()).navigate(action);
+                    progressBar.visibility = View.GONE
                 } else {
+                    progressBar.visibility = View.GONE
                     Toast.makeText(this.context, "Error logging in", Toast.LENGTH_SHORT).show()
                 }
             }
